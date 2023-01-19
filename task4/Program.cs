afe5697634dc7d73e51428589fb44d6b857476ab
Anton Tarasov <120733835+antvd@users.noreply.github.com>
2023-01-19 09:55:41 +0600

    добавлено условие задачи

--- task4/Program.cs
+++ task4/Program.cs
@@ -1,4 +1,8 @@
-﻿Console.WriteLine("Введите целое число:");
+// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+Console.WriteLine("Введите целое число:");
 int number = int.Parse(Console.ReadLine());
 Console.WriteLine();
 if(number >= 1){
