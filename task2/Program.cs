142c62fb0127addb455d59d614ddca7ae3ee2475
Anton <ttarasovanton@ya.ru>
2023-01-19 09:49:41 +0600

    initial commit

---
+++ task2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.WriteLine("Введите первое число:");
+int numberA = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число:");
+int numberB = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите третее число:");
+int numberС = int.Parse(Console.ReadLine());
+int max = numberA;
+
+if (numberB > max){
+    max = numberB;
+}
+if (numberС > max){
+    max = numberС;
+}
+Console.WriteLine("Максимальное число: ");
+Console.Write(max);
---
+++ task4/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Введите целое число:");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine();
+if(number >= 1){
+    for(int i = number; i >= 1; --i){
+        Console.WriteLine(i);
+    }
+}
+else
+{
+    for(int i = number; i <= 1; i++){
+        Console.WriteLine(i);
+    }
+}
